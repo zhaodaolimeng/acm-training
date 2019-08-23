630ca236b81b672b47ebecab7f90b31a9ab167af
Meng Li <zhaodaolimeng@gmail.com>
2019-08-23 21:37:07 +0800

    0823

--- codeforces/1-100/5D-FollowTrafficRules.cpp
+++ codeforces/1-100/5D-FollowTrafficRules.cpp
@@ -18,48 +18,57 @@
 #include <iomanip>
 using namespace std;
 
-// 5 70
-// 200 170 40  
 
 int main(){
     
-    int a,v;
-    int l,d,w;
+    double a,v;
+    double l,d,w;
     cin>>a>>v;
     cin>>l>>d>>w;
     
-    double ret=0, v_;
-    
-    if(v<=w||2*d*a<=w*w){
-        if(0.5*v*v/a>=l){
-            ret = sqrt(2*l/a);
+    double t1, t2, v_, vd;
+
+    // 前半段
+    if(v<=w){ // 最高车速小于限速
+        if(2*d*a <= v*v) {
+            t1 = sqrt(2.0*d/a);
+            vd = sqrt(2.0*d*a);
         }else{
-            ret = 1.0*v/a;
-            ret += (l-0.5*v*v/a)/v;
+            t1 = v/a + (d-v*v*0.5/a)/v;
+            vd = v;
         }
+    }else if(2*d*a <= w*w){ // 标志之前没有到达限速
+        t1 = sqrt(2.0*d/a);
+        vd = sqrt(2.0*d*a);
     }else{
-        if(2.0*a*d<=v*v){
-            v_=sqrt(1.0*a*d+0.5*w*w);
-            ret += (2.0*v_-w)/a;            
-            if(0.5*(v*v-w*w)/a>=l-d){                
-                ret += (sqrt(2*a*(l-d)+w*w)-w)/a;
-            }else{
-                ret += (1.0*v-w)/a;
-                ret += 1.0*(l-d-0.5*(v*v-w*w)/a)/v + (1.0*v-w)/a;                
-            }
-        }else if(2.0*a*(l-d)>=1.0*w*w-v*v){
-            ret += 1.0*w/a+v/a+(d-(v*v-0.5*w*w)/a)/v;
-            ret += 1.0*(v-w)/a;
-            ret += 1.0*(l-d-0.5*(v*v-w*w)/a)/v;            
-        }else{
-            ret += 1.0*(d-(v*v-w*w*0.5)/a)/v;
-            ret += 1.0*v/a;
-            ret += 1.0*(v-w)/a;
-            ret += (sqrt(2.0*a*(l-d)+w*w)-w)/a;
+        v_ = sqrt(a*d+0.5*w*w);  // v_为前半段达到的最高速度
+        if(v >= v_ && v_ >= w){  // 没有加满速之前开始降速通过标志
+            t1 = (2.0*v_-w)/a;
+        }else{  // 加满速之后降速通过标志
+            t1 = v/a;  // 加速
+            t1 += (d-(v*v-0.5*w*w)/a)/v;  // 满速
+            t1 += (v-w)/a;  // 减速
         }
-    }    
+        vd = w;
+    }
+
+    // 后半段
+    if(2.0*a*(l-d) <= v*v-vd*vd){  // 没加满速就到达了终点
+        t2 = (sqrt(2.0*a*(l-d)+vd*vd)-vd)/a;
+    }else{
+        t2 = (v-vd)/a;
+        t2 += ((l-d)-0.5*(v*v/a-vd*vd/a))/v;
+    }
+
+    // cout<<t1<<" "<<t2<<endl;
+
     cout<<fixed;
-    cout<<setprecision(6)<<ret<<endl;
-    //system("PAUSE");
+    cout<<setprecision(8)<<t1+t2<<endl;
     return 0;
 }
+
+/*
+7143 4847
+4193 2677 1991
+1.00753 0.430567
+*/
